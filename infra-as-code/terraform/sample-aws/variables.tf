0d33f79c2c8dbf9ba66c36d0eae9eb7bae992dc4
sahmedniua <sahmed@niua.org>
2023-12-18 22:08:24 +0530

    Upyog

--- infra-as-code/terraform/sample-aws/variables.tf
+++ infra-as-code/terraform/sample-aws/variables.tf
@@ -5,7 +5,7 @@
 
 variable "cluster_name" {
   description = "Name of the Kubernetes cluster"
-  default = <cluster_name> #REPLACE
+  default = "upyogpet" #REPLACE
 }
 
 variable "vpc_cidr_block" {
@@ -47,25 +47,25 @@ variable "number_of_worker_nodes" {
 
 variable "ssh_key_name" {
   description = "ssh key name, not required if your using spot instance types"
-  default = <ssh_key_name> #REPLACE
+  default = "upyogpet-ssh-key" #REPLACE
 }
 
 
 variable "db_name" {
   description = "RDS DB name. Make sure there are no hyphens or other special characters in the DB name. Else, DB creation will fail"
-  default = <db_name> #REPLACE
+  default = "upyogpet" #REPLACE
 }
 
 variable "db_username" {
   description = "RDS database user name"
-  default = <db_username> #REPLACE
+  default = "upyogpet" #REPLACE
 }
 
 #DO NOT fill in here. This will be asked at runtime
 variable "db_password" {}
 
 variable "public_key" {
-  default = <public_ssh_key>
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCHj7/5xmJlceMPmHyTN1bc96lNFO2NRWUccdhv7S0ZzPZ4qnlQTvYoT/HAK2nYIRFmn2ml0/dpjOmW1NruHv7aB6WUZKZwOnLF24z/WubC1I904OPhrkLemalhP807vzI+dn1R7wEmDWJcOye1BPHICv30VSrubR+4o7qBejWQuLfMhBMmDZ7ijCxFpfplnJxggZnl8SbXggtzEfobdrIpzozx30CrcB2YBz2VYpQsQALC/ipfReBBVUbYdWAEq0NkUArANqJla3hVgJkOyBaBgBegYpQmQgTeUY0lvbTEwF0Tabk4JnwL3z3FF0IibKVXPwMe4KoiL+bjSqj7XaqR"
   description = "ssh key"
 }
 
